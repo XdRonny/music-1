26e28b634fa7b1dda3abcf258c3664f86faceb2a
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 10:32:39 +0530

    kannadiga

--- kannadiga/core/userbot.py
+++ kannadiga/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Hindichat_house")
-                await self.one.join_chat("ROMEO_NETWORK")
+                await self.one.join_chat("Do_jism_ek_jaan_op")
+                await self.one.join_chat("Kannada_chatbox")
             except:
                 pass
             assistants.append(1)
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Hindichat_house")
-                await self.two.join_chat("ROMEO_NETWORK")
+                await self.two.join_chat("Do_jism_ek_jaan_op")
+                await self.two.join_chat("Kannada_chatbox")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Hindichat_house")
-                await self.three.join_chat("ROMEO_NETWORK")
+                await self.three.join_chat("Do_jism_ek_jaan_op")
+                await self.three.join_chat("Kannada_chatbox")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Hindichat_house")
-                await self.four.join_chat("ROMEO_NETWORK")
+                await self.four.join_chat("Do_jism_ek_jaan_op")
+                await self.four.join_chat("Kannada_chatbox")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Hindichat_house")
-                await self.five.join_chat("ROMEO_NETWORK")
+                await self.five.join_chat("Do_jism_ek_jaan_op")
+                await self.five.join_chat("Kannada_chatbox")
             except:
                 pass
             assistants.append(5)
