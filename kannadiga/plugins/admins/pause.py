69f72883975c7449db10f50a387cbf30b55da366
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 10:44:06 +0530

    kannadiga

--- kannadiga/plugins/admins/pause.py
+++ kannadiga/plugins/admins/pause.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from ShizukaXMusic import app
-from ShizukaXMusic.core.call import Shizuka
-from ShizukaXMusic.utils.database import is_music_playing, music_off
-from ShizukaXMusic.utils.decorators import AdminRightsCheck
+from kannadiga import app
+from kannadiga.core.call import Shizuka
+from kannadiga.utils.database import is_music_playing, music_off
+from kannadiga.utils.decorators import AdminRightsCheck
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
@@ -22,7 +22,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"], disable_web_page_preview=True)
     await music_off(chat_id)
-    await Shizuka.pause_stream(chat_id)
+    await kannadigabot.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), disable_web_page_preview=True
     )
