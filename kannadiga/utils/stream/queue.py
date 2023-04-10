59b0595aeb9829ef8a69109d5b92cb00dc2404fe
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 11:38:42 +0530

    kannadiga

--- kannadiga/utils/stream/queue.py
+++ kannadiga/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from ShizukaXMusic.misc import db
+from kannadiga.misc import db
 
 
 async def put_queue(
