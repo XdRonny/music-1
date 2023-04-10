8d4255ddff01debf712d887f4fbd038c9feb544d
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 10:17:40 +0530

    Update __main__.py

--- kannadiga/__main__.py
+++ kannadiga/__main__.py
@@ -23,7 +23,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("ShizukaXMusic").error("Add Pyrogram string session and then try...")
+        LOGGER("kannadiga").error("Add Pyrogram string session and then try...")
     try:
         users = await get_gbanned()
         for user_id in users:
