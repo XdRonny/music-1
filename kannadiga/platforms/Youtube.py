38605da04f58e3bb00df1d2e9f5f69d492a80a2e
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 10:40:03 +0530

    kannadiga

--- kannadiga/platforms/Youtube.py
+++ kannadiga/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from ShizukaXMusic.utils.database import is_on_off
-from ShizukaXMusic.utils.formatters import time_to_seconds
+from kannadiga.utils.database import is_on_off
+from kannadiga.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
