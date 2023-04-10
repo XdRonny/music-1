bb59dd0e4ebe90fd4354c1c749034254d3431862
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-10 10:46:52 +0530

    kannadiga

--- kannadiga/plugins/admins/stop.py
+++ kannadiga/plugins/admins/stop.py
@@ -3,11 +3,11 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from ShizukaXMusic import app
+from kannadiga import app
 
-from ShizukaXMusic.core.call import Shizuka
-from ShizukaXMusic.utils.database import set_loop
-from ShizukaXMusic.utils.decorators import AdminRightsCheck
+from kannadiga.core.call import Shizuka
+from kannadiga.utils.database import set_loop
+from kannadiga.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -20,7 +20,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Shizuka.stop_stream(chat_id)
+    await kannadigabot.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention), disable_web_page_preview=True
